64a38a620cccf651260d0380540112d3d37b8509
prem-io <PremK.Lf@gmail.com>
2020-04-04 12:51:15 +0530

    chore: package upgrade

---
+++ src/components/BookInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class BookInfo extends Component {
+  render() {
+    return (
+      <div>
+        BookInfo
+      </div>
+    )
+  }
+}
+
+export default BookInfo
